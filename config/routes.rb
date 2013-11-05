8dd773925f637cd379e0f2c4ccfd21825f3dc65c
tesstryan <tesstryan@gmail.com>
2013-11-04 20:20:09 -0600

    Updated font

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ FavMoviesApp::Application.routes.draw do
   post '/posts' => 'posts#create'
   get '/posts/new' => 'posts#new', :as => 'new_post'
 
+  get '/posts/:id' => 'posts#show', :as => 'post'
+
   get '/posts/:id/edit' => 'posts#edit', :as => 'edit_post'
   patch '/posts/:id' => 'posts#update'
-
-  get '/posts/:id' => 'posts#show', :as => 'post'
 
   delete '/posts/:id' => 'posts#destroy'
 
